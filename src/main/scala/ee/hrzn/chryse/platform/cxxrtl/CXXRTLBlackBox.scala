563a4ca1ef3bc0efa9ef22a0769126235393576c
Asherah Connor <ashe@kivikakk.ee>
2024-05-16 13:52:48 +0300

    CXXRTLBlackBox: just use BlackBox directly.

--- src/main/scala/ee/hrzn/chryse/platform/cxxrtl/CXXRTLBlackBox.scala
+++
@@ -1,5 +0,0 @@
-package ee.hrzn.chryse.platform.cxxrtl
-
-import chisel3.BlackBox
-
-abstract class CXXRTLBlackBox extends BlackBox {}
--- src/main/scala/ee/hrzn/chryse/platform/cxxrtl/CXXRTLOptions.scala
+++ src/main/scala/ee/hrzn/chryse/platform/cxxrtl/CXXRTLOptions.scala
@@ -1,6 +1,8 @@
 package ee.hrzn.chryse.platform.cxxrtl
 
+import chisel3.BlackBox
+
 final case class CXXRTLOptions(
     clockHz: Int,
-    blackboxes: Seq[Class[_ <: CXXRTLBlackBox]] = Seq(),
+    blackboxes: Seq[Class[_ <: BlackBox]] = Seq(),
 )
