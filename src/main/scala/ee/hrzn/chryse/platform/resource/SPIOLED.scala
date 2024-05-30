290643e5211c5f866bc51712ab158b24fdb6a748
Asherah Connor <ashe@kivikakk.ee>
2024-05-30 13:09:05 +0300

    put sbt on PATH; wip.

--- src/main/scala/ee/hrzn/chryse/platform/resource/SPIOLED.scala
+++ src/main/scala/ee/hrzn/chryse/platform/resource/SPIOLED.scala
@@ -4,20 +4,22 @@ import chisel3._
 import chisel3.experimental.Param
 
 class SPIOLED extends ResourceBase {
-  val cs    = ResourceData(Output(Bool()), invert = true)
+  // TODO: There's often a BL/BLK pin pulled high by the OLED which you can
+  // ground to turn off the backlight.
+  val cs    = ResourceData(Output(Bool()), invert = true) // permitted to be unset
   val clock = ResourceData(Output(Clock()))
   val copi  = ResourceData(Output(Bool()))
-  val cipo  = ResourceData(Input(Bool()))
-  val wp    = ResourceData(Output(Bool()), invert = true)
-  val hold  = ResourceData(Output(Bool()), invert = true)
+  val cipo  = ResourceData(Input(Bool()))                 // permitted to be unset
+  val dc    = ResourceData(Output(Bool()), invert = true)
+  val res   = ResourceData(Output(Bool()), invert = true)
 
   def setName(name: String): Unit = {
     cs.setName(s"${name}_cs")
     clock.setName(s"${name}_clock")
     copi.setName(s"${name}_copi")
     cipo.setName(s"${name}_cipo")
-    wp.setName(s"${name}_wp")
-    hold.setName(s"${name}_hold")
+    dc.setName(s"${name}_dc")
+    res.setName(s"${name}_res")
   }
 
   def withAttributes(attribs: (String, Param)*): this.type = {
@@ -25,8 +27,8 @@ class SPIOLED extends ResourceBase {
     clock.withAttributes(attribs: _*)
     copi.withAttributes(attribs: _*)
     cipo.withAttributes(attribs: _*)
-    wp.withAttributes(attribs: _*)
-    hold.withAttributes(attribs: _*)
+    dc.withAttributes(attribs: _*)
+    res.withAttributes(attribs: _*)
     this
   }
 
@@ -35,29 +37,31 @@ class SPIOLED extends ResourceBase {
     clock.setDefaultAttributes(defaultAttributes)
     copi.setDefaultAttributes(defaultAttributes)
     cipo.setDefaultAttributes(defaultAttributes)
-    wp.setDefaultAttributes(defaultAttributes)
-    hold.setDefaultAttributes(defaultAttributes)
+    dc.setDefaultAttributes(defaultAttributes)
+    res.setDefaultAttributes(defaultAttributes)
   }
 
   def onPins(
-      csN: Pin,
+      csN: Pin = null,
       clock: Pin,
       copi: Pin,
-      cipo: Pin,
-      wpN: Pin,
-      holdN: Pin,
+      cipo: Pin = null,
+      dcN: Pin,
+      resN: Pin,
   ): this.type = {
-    this.cs.onPin(csN)
+    if (csN != null)
+      this.cs.onPin(csN)
     this.clock.onPin(clock)
     this.copi.onPin(copi)
-    this.cipo.onPin(cipo)
-    this.wp.onPin(wpN)
-    this.hold.onPin(holdN)
+    if (cipo != null)
+      this.cipo.onPin(cipo)
+    this.dc.onPin(dcN)
+    this.res.onPin(resN)
     this
   }
 
   def data: Seq[ResourceData[_ <: Data]] =
-    Seq(cs, clock, copi, cipo, wp, hold)
+    Seq(clock, copi, dc, res) ++ Seq(cs, cipo).filter(_.pinId.isDefined)
 }
 
 object SPIOLED {
